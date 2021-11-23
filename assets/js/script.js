2cea47b82dd52606568643c5194b8bcb269e631e
Steven Cruz <cruz801@hotmail.com>
2021-11-22 22:56:46 -0700

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,6 +63,7 @@ var auditTask = function(taskEl) {
 
  $("#modalDueDate").datepicker({
   minDate: 1
+  
 });   //added from bottom of page
 
 
@@ -80,7 +81,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -250,15 +251,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
     //console.log("activate", this);
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
     //console.log("deactivate", this);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active")
     //console.log("over", event.target);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active")
     //console.log("out", event.target);
   },
 
@@ -303,22 +310,25 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active")
     //remove element from the DOM
     ui.draggable.remove();
     //console.log("drop");
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("ui");
   },
   out: function(event, ui){
+    $(".bottom-trash").removeClass("bottom-trash-active")
     console.log("ui");
 
     
   }
 });
 
-
-// $("#modalDueDate").datepicker({ just commented ou
-//   minDate: 1
-// });
-
+setInterval(function(){
+  $(".card .list-group-item").each(function(index,el){
+    auditTask(el);
+  });
+},(1000 * 60) *30);
